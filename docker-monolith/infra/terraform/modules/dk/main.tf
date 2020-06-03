be9ad13101110b828d0d453d303032bc6137a297
Denis Lunev <hataldir@gmail.com>
2020-06-03 08:14:39 +0500

    HW18

--- docker-monolith/infra/terraform/modules/dk/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">0.12.8"
-}
-
-provider "google" {
-  version = "~>2.15"
-  zone    = var.zone
-  project = var.project
-}
--- docker-monolith/infra/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "dk_external_ip" {
-  value = module.dk[*].dk_external_ip
-}
-
